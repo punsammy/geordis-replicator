ef8c3c1770e626ed620963d5b778f45e23410773
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-05 10:37:04 -0400

    episode3

--- geordis-replicator.rb
+++ geordis-replicator.rb
@@ -1,3 +1,18 @@
 require_relative 'base/app'
 
 App.new
+
+
+# def initialize
+#
+#   command = ARGV.first
+#   Display.help_and_exit unless command
+#
+#   if command.slice(0, 2) == '--'
+#     Generator.invoke(command)
+#   else
+#     episode_number = pad_with_zero(command)
+#     play(episode_number)
+#   end
+#
+# end
